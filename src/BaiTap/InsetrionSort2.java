f05af2c097ab84c19fd8fb76968d45b99e25c660
MinhHoang3011 <minhhoang301198@gmail.com>
2022-07-20 14:32:21 +0700

    Cài đặt thuật toán sắp xếp chèn

---
+++ src/BaiTap/InsetrionSort2.java
@@ -0,0 +1,24 @@
+package BaiTap;
+
+import javafx.scene.chart.BubbleChart;
+
+import java.util.Arrays;
+
+public class InsetrionSort2 {
+    public static void main(String[] args) {
+        int[] arrayInts = {2, 5, 6, 323, 66, 81, 23, 56};
+        incertionSort(arrayInts);
+    }
+
+    public static void incertionSort(int[] a) {
+        for (int i = 1; i < a.length; i++) {
+            int ai = a[i];
+            int j = i - 1;
+            while (j >= 0 && a[j] > ai) {
+                a[j + 1] = a[j];
+                j--;
+            }
+            a[j + 1] = ai;
+        }
+    }
+}
